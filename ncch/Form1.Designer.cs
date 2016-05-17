a615d6c64abc02334d3027082942544805b2082e
LinChangHan <F74036386@mail.ncku.edu.tw>
2016-05-17 23:26:56 +0800

    add label in tabel and Gui init

--- ncch/Form1.Designer.cs
+++ ncch/Form1.Designer.cs
@@ -28,19 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonUpdate
             // 
-            this.button1.Location = new System.Drawing.Point(691, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "update";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Location = new System.Drawing.Point(889, 12);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 0;
+            this.buttonUpdate.Text = "update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -55,50 +65,147 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 13F));
             this.tableLayoutPanel1.Location = new System.Drawing.Point(23, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 12;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.347826F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.695652F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(415, 437);
+            this.tableLayoutPanel1.RowCount = 13;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(415, 485);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(483, 206);
+            this.listBox1.Location = new System.Drawing.Point(474, 154);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(268, 232);
+            this.listBox1.Size = new System.Drawing.Size(466, 328);
             this.listBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(511, 70);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(76, 20);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(626, 70);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(72, 20);
+            this.comboBox3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "科系";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(635, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "年級";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(754, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "班級";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(733, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(77, 20);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(889, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "匯入必修";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(607, 128);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(164, 20);
+            this.comboBox4.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "查看科系";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 494);
+            this.ClientSize = new System.Drawing.Size(976, 494);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonUpdate);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonUpdate;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- ncch/Form1.cs
+++ ncch/Form1.cs
@@ -12,9 +12,105 @@ namespace ncch
 {
     public partial class Form1 : Form
     {
+        Label[] index;
+        Label[] Monday;
+        Label[] Tuesday;
+        Label[] Wednesday;
+        Label[] Thursday;
+        Label[] Friday;
+        Label[] Saturday;
+        Label[] Sunday;
+
+        Label[][] classTable;
+    
         public Form1()
         {
             InitializeComponent();
+            initialTable();
+        }
+
+        private void initialTable()
+        {           
+            //  initial the table of class
+            
+            classTable=new Label [8][];
+
+            for(int i=0;i<8;i++){
+                classTable[i]=new Label[13];
+                for(int j=0;j<13;j++){
+                    classTable[i][j]=new Label();
+                    classTable[i][j].Visible=true;
+                    classTable[i][j].BackColor=Color.White;
+                    this.tableLayoutPanel1.Controls.Add(this.classTable[i][j],i,j);
+                    classTable[i][j].Text=i.ToString()+","+j.ToString();
+                }
+            }
+
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetDouble;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            /*   index=new Label[13];                       //creat label array
+             Monday=new Label[13];
+             Tuesday = new Label[13];
+             Wednesday = new Label[13];
+             Thursday = new Label[13];
+             Friday = new Label[13];
+             Saturday = new Label[13];
+             Sunday = new Label[13];
+            
+            for (int i = 0; i < 13; i++)
+             {  
+              
+                 index[i] = new Label();                    //creat label
+                 Monday[i] = new Label();
+                 Tuesday[i] = new Label();
+                 Wednesday[i] = new Label();
+                 Thursday[i] = new Label();
+                 Friday[i] = new Label();
+                 Saturday[i] = new Label();
+                 Sunday[i] = new Label();
+
+                 index[i].Visible = true;                        //set visible of label
+                 Monday[i].Visible = true;
+                 Tuesday[i].Visible = true;
+                 Wednesday[i].Visible = true;
+                 Thursday[i].Visible = true;
+                 Friday[i].Visible = true;
+                 Saturday[i].Visible = true;
+                 Sunday[i].Visible = true;
+            
+                 index[i].BackColor = Color.Yellow;                 // set color to label
+                 Monday[i].BackColor = Color.Yellow;
+                 Tuesday[i].BackColor = Color.Yellow;
+                 Wednesday[i].BackColor = Color.Yellow;
+                 Thursday[i].BackColor = Color.Yellow;
+                 Friday[i].BackColor = Color.Yellow;
+                 Saturday[i].BackColor = Color.Yellow;
+                 Sunday[i].BackColor = Color.Yellow;
+
+                 this.tableLayoutPanel1.Controls.Add(this.index[i], 0, i);       //add label to table
+                 this.tableLayoutPanel1.Controls.Add(this.Monday[i], 1, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Tuesday[i], 2, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Wednesday[i], 3, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Thursday[i], 4, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Friday[i] ,5, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Saturday[i], 6, i);
+                 this.tableLayoutPanel1.Controls.Add(this.Sunday[i], 7, i);
+             }
+             * */
+
+        }
+        
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
