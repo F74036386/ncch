d7a86ea94371f38d73ae1c792de321edf0adc89b
wwolfyTC <tim011919@gmail.com>
2016-05-19 10:35:06 +0800

    fatch debuged

--- ncch/Form1.cs
+++ ncch/Form1.cs
@@ -16,16 +16,6 @@ namespace ncch
 {
     public partial class Form1 : Form
     {
-        /*
-         Label[] index;
-         Label[] Monday;
-         Label[] Tuesday;
-         Label[] Wednesday;
-         Label[] Thursday;
-         Label[] Friday;
-         Label[] Saturday;
-         Label[] Sunday;
-        */
         Label[][] classTable;
         string mainUrl;
 
@@ -46,8 +36,6 @@ namespace ncch
             comboBoxClass.Items.Add("甲班");
             comboBoxClass.Items.Add("乙班");
             comboBoxClass.Items.Add("丙班");
-
-            //  comboBoxDepartment.Items.Add();
 
         }
 
@@ -93,62 +81,10 @@ namespace ncch
 
             setComboBox();
 
-            /*   index=new Label[13];                       //creat label array
-             Monday=new Label[13];
-             Tuesday = new Label[13];
-             Wednesday = new Label[13];
-             Thursday = new Label[13];
-             Friday = new Label[13];
-             Saturday = new Label[13];
-             Sunday = new Label[13];
-            
-            for (int i = 0; i < 13; i++)
-             {  
-              
-                 index[i] = new Label();                    //creat label
-                 Monday[i] = new Label();
-                 Tuesday[i] = new Label();
-                 Wednesday[i] = new Label();
-                 Thursday[i] = new Label();
-                 Friday[i] = new Label();
-                 Saturday[i] = new Label();
-                 Sunday[i] = new Label();
-
-                 index[i].Visible = true;                        //set visible of label
-                 Monday[i].Visible = true;
-                 Tuesday[i].Visible = true;
-                 Wednesday[i].Visible = true;
-                 Thursday[i].Visible = true;
-                 Friday[i].Visible = true;
-                 Saturday[i].Visible = true;
-                 Sunday[i].Visible = true;
-            
-                 index[i].BackColor = Color.Yellow;                 // set color to label
-                 Monday[i].BackColor = Color.Yellow;
-                 Tuesday[i].BackColor = Color.Yellow;
-                 Wednesday[i].BackColor = Color.Yellow;
-                 Thursday[i].BackColor = Color.Yellow;
-                 Friday[i].BackColor = Color.Yellow;
-                 Saturday[i].BackColor = Color.Yellow;
-                 Sunday[i].BackColor = Color.Yellow;
-
-                 this.tableLayoutPanel1.Controls.Add(this.index[i], 0, i);       //add label to table
-                 this.tableLayoutPanel1.Controls.Add(this.Monday[i], 1, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Tuesday[i], 2, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Wednesday[i], 3, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Thursday[i], 4, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Friday[i] ,5, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Saturday[i], 6, i);
-                 this.tableLayoutPanel1.Controls.Add(this.Sunday[i], 7, i);
-             }
-             * */
-
         }
 
         private void buttonUpdate_Click(object sender, EventArgs e)
         {
-            StreamWriter sw = new StreamWriter(@"./classhtml.txt");
-
             //fatchMenu();
             fatchCourse("A9");
         }
@@ -280,7 +216,7 @@ namespace ncch
                     switch (++i)
                     {
                         case 3:
-                            id = findHtmlTag.Replace(cur, "");
+                            id = (string)e.Argument + findHtmlTag.Replace(cur, "");
                             break;
                         case 6:
                             cls = findHtmlTag.Replace(cur, ""); ;
@@ -314,7 +250,7 @@ namespace ncch
                         case 18:
                             other = findHtmlTag.Replace(cur, "");
                             break;
-                        case 20:
+                        case 23:
                             i = 0;
                             Console.WriteLine(id + "\t" + cls + "\t" + grade + "\t" + type + "\t" + english + "\t" + name + "\t" + necessary + "\t" + point + "\t" + teacher + "\t" + time + "\t" + place + "\t" + other);
                             break;
